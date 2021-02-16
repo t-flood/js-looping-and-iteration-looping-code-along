f23bad873729fd5cdb3df9cc5ceb59573efdcd41
Tim Flood <tflood9@gmail.com>
2021-02-16 11:05:42 -0500

    commit tree 9503e8a9bfdf471cf9032ce0a213751f215dc047

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(cards, event) {
+  const thankYouCards = [];
+  for (let i = 0; i < cards.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${cards[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouCards;
+}
+
+function countDown() {
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
